21eb38f8c424e1962bf375d9e98bbb2669b6640b
Diego Cano <diegocano044@gmail.com>
2021-03-03 15:33:24 -0600

    Subiendo los primeros archivos

---
+++ HT1.py
@@ -0,0 +1,4 @@
+Peso = input("Ingresa tu peso, de preferencia en Kg")
+Estatura = input("Ingresa tu altura, de preferencia en metros")
+IMC = round((peso)/(Estatura)**2)
+print("Tu indice de masa corporal es" , IMC + "Si tu IMC esta de 18.5 a 24.9 estas en lo normal" )
